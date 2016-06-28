ec776cd31c9951b8df7315a16d088bb34e27d374
曹巍 <tsaowe@gmail.com>
2016-06-28 11:41:30 +0800

    change

--- index.ios.js
+++ index.ios.js
@@ -12,42 +12,36 @@ import {
   View
 } from 'react-native';
 
-class jumeiapp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+const jumeiapp = React.createClass({
+    render: function () {
+        return (
+            <View style={styles.container}>
+                
+                <Text style={styles.instructions}>
+                    nihao 中文
+                </Text>
+            </View>
+        );
+    }
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
 
 AppRegistry.registerComponent('jumeiapp', () => jumeiapp);
