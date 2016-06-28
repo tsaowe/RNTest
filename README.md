beed449e65d888fc26c5d32f7de8991c268119a7
曹巍 <tsaowe@gmail.com>
2016-06-28 10:35:30 +0800

    2016年06月28日10:35:29

---
+++ README.md
@@ -0,0 +1,7 @@
+1 react-native init jumeiapp
+2 cp changeIp.py
+3 add readme.md
+4 open xproj -a Xcode
+5 jumeiapp->General->bundle Identifier ->com.tsaowe
+6 python changeIp.py
+7 run on iphone
--- changeIp.py
+++ changeIp.py
@@ -7,14 +7,14 @@ if __name__ == '__main__':
     pat = re.compile(r'(?<![\.\d])(?:\d{1,3}\.){3}\d{1,3}(?![\.\d])|localhost')
     ips = pat.findall(output)
     currentip = ips[0]
-    file_object = open('ios/RCTJumei/AppDelegate.m')
+    file_object = open('ios/jumeiapp/AppDelegate.m')
     try:
          all_the_text = file_object.read()
          searchIps = pat.findall(all_the_text)
          searchIp = searchIps[0]
          all_the_text = all_the_text.replace(searchIp, currentip)
          # print all_the_text
-         f = codecs.open('ios/RCTJumei/AppDelegate.m','w','utf-8')
+         f = codecs.open('ios/jumeiapp/AppDelegate.m','w','utf-8')
          f.write(all_the_text)
          f.close()
     finally:
