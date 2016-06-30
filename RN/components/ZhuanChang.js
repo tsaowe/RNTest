a1b42a3c52c462223b139b5bf8db02d79105c36f
曹巍 <tsaowe@gmail.com>
2016-06-30 16:36:11 +0800

    2016年06月30日16:36:09

--- RN/components/ZhuanChang.js
+++ RN/components/ZhuanChang.js
@@ -3,25 +3,97 @@
  */
 
 import React from 'react';
-import {
+var ReactNative = require('react-native');
+let {
     StatusBar,
-    View
-} from 'react-native';
+    View,
+    Image,
+    ListView,
+    StyleSheet,
+    Dimensions,
+    TouchableOpacity
+} = ReactNative;
 
 import Piaochuang from './piaochuang';
-import List from './listView';
+import Product from './product';
 
-import GoTop from './goTop';
+
+let { width} = Dimensions.get('window');
+let goTopWidth = width / 6;
+
+
+
+
+const listViewStyles = StyleSheet.create({
+    list: {
+        justifyContent: 'space-around',
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    }
+});
+
+const goTopStyles = StyleSheet.create({
+
+    image: {
+        width: goTopWidth,
+        height: goTopWidth,
+        borderRadius: goTopWidth / 2
+    },
+    container: {
+        position:'absolute',
+        bottom:goTopWidth,
+        transform: [{'translate': [0, 0, 1]}],
+        right:goTopWidth * .6
+    }
+});
+
+
+
 
 export default React.createClass({
+
+    getInitialState: function () {
+        var ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+        return {
+            dataSource: ds.cloneWithRows(this._genRows({}))
+        };
+    },
+    
     render: function () {
         return (
             <View style={{flex:1}}>
                 <StatusBar hidden={true}/>
                 <Piaochuang />
-                <List/>
-                <GoTop/>
+                <ListView
+                    ref="list"
+                    contentContainerStyle={listViewStyles.list}
+                    dataSource={this.state.dataSource}
+                    initialListSize={21}
+                    pageSize={2}
+                    scrollRenderAheadDistance={500}
+                    renderRow={this._renderRow}
+                />
+                <TouchableOpacity
+                    activeOpacity={.7}>
+                    <View style={[goTopStyles.container,goTopStyles.image]}>
+                        <Image style={goTopStyles.image} source={{uri:'http://mp3.jmstatic.com/q_80/jm_img/gotop.png'}}/>
+                    </View>
+                </TouchableOpacity>
             </View>
         );
+    },
+    _renderRow: function () {
+        return (
+            <Product/>
+        );
+    },
+    _genRows: function () {
+        var dataBlob = [];
+        for (var ii = 0; ii < 30; ii++) {
+            dataBlob.push(ii);
+        }
+        return dataBlob;
     }
 });
